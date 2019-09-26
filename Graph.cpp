723f8b47a55e58826b93953190c19e173d491c82
oriolpascual9 <43992574+oriolpascual9@users.noreply.github.com>
2019-09-26 10:04:52 +0200

    first commit

---
+++ Graph.cpp
@@ -0,0 +1,224 @@
+/**
+ * @file    Graph.cpp
+ * @brief   Implementació de la classe Graph.
+ */
+
+#include "Graph.h"
+
+Graph::Graph(int maxId) {
+    this->maxId = maxId;
+    maxIdActiu = -1;
+    minIdActiu = -1;
+    nV = 0;
+    nA = 0;
+    nodeLG n;
+    n.actiu = false;
+    n.adjacencies = set<int>();
+    nodes = vector<nodeLG>(maxId + 1, n);
+}
+
+Graph Graph::generateActivatedGraph(int nV) {
+    Graph lG(nV - 1);
+    lG.maxIdActiu = nV - 1;
+    lG.minIdActiu = 0;
+    lG.nV = nV;
+    nodeLG n;
+    n.actiu = true;
+    n.adjacencies = set<int>();
+    lG.nodes = vector<nodeLG>(nV, n);
+    return lG;
+}
+
+Graph Graph::generateERGraph(int nV, int M) {
+    Graph lG = generateActivatedGraph(nV);
+    for (int i = 0; i < M; ++i) {
+        int id1 = rand() % nV;
+        int id2 = rand() % nV;
+        if (id1 != id2 and not lG.adjacent(id1, id2))
+            lG.addAresta(id1, id2);
+        else
+            --i;
+    }
+    return lG;
+}
+
+void Graph::addVertex(int id) {
+    if (id > maxId)
+        cout << "La id " << id << " és més alta que maxId." << endl;
+    else if (isActive(id))
+        cout << "Ya existeix un vertex amb la id " << id << '.' << endl;
+    else {
+        if (maxIdActiu == -1) {
+            maxIdActiu = id;
+            minIdActiu = id;
+        }
+        else {
+            if (id > maxIdActiu)
+                maxIdActiu = id;
+            else if (id < minIdActiu)
+                minIdActiu = id;
+        }
+        ++nV;
+        nodes[id].actiu = true;
+    }
+}
+
+void Graph::removeVertex(int id) {
+    if (not isActive(id))
+        cout << "No existeix un vertex amb la id " << id << '.' << endl;
+    else {
+        if (maxIdActiu == id and minIdActiu == id) {
+            maxIdActiu = -1;
+            minIdActiu = -1;
+        }
+        else if (maxIdActiu == id) {
+            bool found = false;
+            for (int i = maxIdActiu - 1; i >= minIdActiu and not found; --i)
+                if (nodes[i].actiu) {
+                    maxIdActiu = i;
+                    found = true;
+                }
+        }
+        else if (minIdActiu == id) {
+            bool found = false;
+            for (int i = minIdActiu + 1; i <= maxIdActiu and not found; ++i)
+                if (nodes[i].actiu) {
+                    minIdActiu = i;
+                    found = true;
+                }
+        }
+        for (set<int>::iterator it = nodes[id].adjacencies.begin(); it != nodes[id].adjacencies.end(); ++it)
+        {
+            nodes[*it].adjacencies.erase(id);
+        }
+        --nV;
+        nA -= nodes[id].adjacencies.size();
+        nodes[id].adjacencies.clear();
+        nodes[id].actiu = false;
+    }
+}
+
+
+
+void Graph::addAresta(int id1, int id2) {
+    if (not isActive(id1))
+        cout << "No existeix un vertex amb la id " << id1 << '.' << endl;
+    else if (not isActive(id2))
+        cout << "No existeix un vertex amb la id " << id2 << '.' << endl;
+    else if (adjacent(id1, id2))
+        cout << "Els vertexs " << id1 << " i " << id2 << " ja són adjacents." << endl;
+    else if (id1 == id2)
+        cout << "S'ha donat dos cops la mateixa id." << endl;
+    else {
+        ++nA;
+        nodes[id1].adjacencies.insert(id2);
+        nodes[id2].adjacencies.insert(id1);
+    }
+}
+
+void Graph::removeAresta(int id1, int id2) {
+    if (not isActive(id1))
+        cout << "No existeix un vertex amb la id " << id1 << '.' << endl;
+    else if (not isActive(id2))
+        cout << "No existeix un vertex amb la id " << id2 << '.' << endl;
+    else if (not adjacent(id1, id2))
+        cout << "Els vertexs " << id1 << " i " << id2 << "no són adjacents." << endl;
+    else {
+        --nA;
+        nodes[id1].adjacencies.erase(id2);
+        nodes[id2].adjacencies.erase(id1);
+    }
+}
+
+void Graph::activate(int id) {
+    if (id > maxId) {
+        cout << "La id " << id << " és més alta que maxId." << endl;
+    }
+    else nodes[id].actiu = true;
+}
+
+
+void Graph::desactivate(int id) {
+    if (id > maxId) {
+        cout << "La id " << id << " és més alta que maxId." << endl;
+    }
+    else nodes[id].actiu = false;
+}
+
+int Graph::getIdDisponible() {
+    for (int i = 0; i <= maxId; ++i)
+        if (not isActive(i))
+            return i;
+    ++maxId;
+    nodeLG n;
+    n.actiu = false;
+    n.adjacencies = set<int>();
+    nodes.push_back(n);
+    return maxId;
+}
+
+int Graph::getNVertexs() { return nV; }
+
+
+set<int> Graph::getVertexs() {
+    set<int> vs = set<int>();
+    for (int i = minIdActiu; i <= maxIdActiu; ++i)
+        if (isActive(i))
+            vs.insert(i);
+    return vs;
+}
+
+int Graph::getNArestes() { return nA; }
+
+
+set<pair<int, int> > Graph::getArestes() {
+    set<pair<int, int> > arestes = set<pair<int, int> >();
+    set<int> vs = getVertexs();
+    vector<bool> visited(maxId + 1, false);
+    for (set<int>::iterator it1 = vs.begin(); it1 != vs.end(); ++it1) {
+        visited[*it1] = true;
+        set<int> adjs = getAdjacencies(*it1);
+        for (set<int>::iterator it2 = adjs.begin(); it2 != adjs.end(); ++it2)
+            if (not visited[*it2]) {
+                pair<int, int> aresta;
+                aresta.first = *it1;
+                aresta.second = *it2;
+                arestes.insert(aresta);
+            }
+    }
+    return arestes;
+}
+
+int Graph::getMaxId() { return maxId; }
+
+
+bool Graph::isActive(int id) {
+    if (id > maxId) {
+        cout << "La id " << id << " és més alta que maxId." << endl;
+        return false;
+    }
+    return nodes[id].actiu;
+}
+
+set<int> Graph::getAdjacencies(int id) {
+    if (not isActive(id)) {
+        cout << "No existeix un vertex amb la id " << id << '.' << endl;
+        return set<int>();
+    }
+    return nodes[id].adjacencies;
+}
+
+bool Graph::adjacent(int id1, int id2) {
+    if (not isActive(id1)) {
+        cout << "No existeix un vertex amb la id " << id1 << '.' << endl;
+        return false;
+    }
+    else if (not isActive(id2)) {
+        cout << "No existeix un vertex amb la id " << id2 << '.' << endl;
+        return false;
+    }
+    if (nodes[id1].adjacencies.size() < nodes[id2].adjacencies.size())
+        return nodes[id1].adjacencies.find(id2) != nodes[id1].adjacencies.end();
+    else
+        return nodes[id2].adjacencies.find(id1) != nodes[id2].adjacencies.end();
+}
---
+++ Graph.h
@@ -0,0 +1,168 @@
+/**
+ * @file    Graph.h
+ * @brief   Especificació de la classe Graph.
+ */
+
+#ifndef GRAPH_H
+#define GRAPH_H
+
+#include <iostream>
+#include <vector>
+#include <set>
+#include <string>
+#include <cmath>
+#include <cstdlib>
+
+using namespace std;
+
+/**Conté la informació d'un possible node del graph.*/
+struct nodeLG {
+    /**Indica si el ListGraph té el node amb la id igual a la posició del vector nodes on es troba el node.*/
+    bool actiu;
+    /**ids dels nodes adjacents al node.*/
+    set<int> adjacencies;
+};
+
+class Graph {
+
+private:
+    int maxId;
+    /**id més alta assignable als nodes del Graph.*/
+    int maxIdActiu;
+    /**id més baixa entre les dels nodes del Graph.*/
+    int minIdActiu;
+    /**Número de vèrtexs que té el Graph.*/
+    int nV;
+    /**Número d'arestes que té el Graph.*/
+    int nA;
+    /**vector que conté els nodes del Graph.*/
+    vector<nodeLG> nodes;
+
+    /**
+     * Genera un Graph amb n vertexs (tots activats) i cap aresta.
+     * @param n Nombre de vèrtexs del Graph.
+     * @return  Graph amb n vertexs (tots activats) i cap aresta.
+     */
+    static Graph generateActivatedGraph(int nV);
+
+public:
+
+    /**Constructores*/
+
+    /**
+     * Crea un Graph buit.
+     * @param maxId Máxima id assignable a un vèrtex del ListGraph.
+     */
+    Graph(int maxId);
+
+    /**
+     * Crea un Graph aleatori donat un nombre de vèrtexs seguint el model Erdős-Rényi.
+     * @param nV    Nombre de vèrtexs que tindrà el Graph.
+     * @param M     Nombre d'arestes del Graph resultant.
+     * @return      Graph aleatori amb nV vèrtexs generat segons el model Erdős-Rényi.
+     */
+    static Graph generateERGraph(int nV, int M);
+
+    /**MODIFICADORES*/
+
+    /**
+     * Afegeix un vèrtex amb la id donada al graph si no existeix cap que ya la tingui.
+     * @param id    id que es vol assignar al vèrtex.
+     */
+    void addVertex(int id);
+
+    /**
+     * Elimina un vèrtex amb la id donada al graph si existeix.
+     * @param id    id qdel vèrtex que es vol eliminar.
+     */
+    void removeVertex(int id);
+
+    /**
+     * Afegeix una aresta entre els vèrtexs amb les ids donades si aquests existeixen i no existeix ya una entre ells.
+     * @param id1   id d'un dels nodes de l'aresta.
+     * @param id2   id d'un dels nodes de l'aresta.
+     */
+    void addAresta(int id1, int id2);
+
+    /**
+     * Elimina una aresta entre els vèrtexs amb les ids donades si aquests existeixen i existex una aresta entre ells.
+     * @param id1   id d'un dels nodes de l'aresta.
+     * @param id2   id d'un dels nodes de l'aresta.
+     */
+    void removeAresta(int id1, int id2);
+
+    /**
+     * Activa el vertex amb la id indicada
+     * @param id    id del vertex a activar
+     */
+    void activate(int id);
+
+    /**
+     * Desactiva el vertex amb la id indicada
+     * @param id    id del vertex a desactivar
+     */
+    void desactivate(int id);
+
+    /**CONSULTORES*/
+
+    /**
+     * Retorna una id disponible per assignar a un vèrtex.
+     * @return  id disponible per assignar a un vèrtex.
+    */
+    int getIdDisponible();
+
+
+    /**
+     * Indica quants vèrtexs té el ListGraph.
+     * @return  Número de vèrtexs que té el ListGraph.
+     */
+    int getNVertexs();
+
+    /**
+     * Retorna totes les ids dels vertexs del ListGraph.
+     * @return  ids dels vertexs del ListGraph.
+     */
+    set<int> getVertexs();
+
+    /**
+     * Indica quantes arestes té el ListGraph.
+     * @return  Número d'arestes que té el ListGraph.
+     */
+    int getNArestes();
+
+    /**
+     * Retorna totes les arestes del ListGraph.
+     * @return  arestes del ListGraph.
+     */
+    set<pair<int, int> > getArestes();
+
+    /**
+     * Retorna la máxima id assignable a un vèrtex del ListGraph.
+     * @return  Máxima id assignable a un vèrtex del ListGraph.
+     */
+    int getMaxId();
+
+    /**
+     * Indica si el ListGraph té un vèrtex amb la id indicada
+     * @param id    id del vèrtex que es vol comprovar.
+     * @return      true si el ListGraph té el vèrtex, false altrament.
+     */
+    bool isActive(int id);
+
+    /**
+     * Retorna la llista d'adjacencies d'un vèrtex amb la id indicada si aquest existeix.
+     * @param id    id del vèrtex del que es vol obtenir la llista d'adjacencies.
+     * @return      llista d'adjacencies del vètex amb la id donada.
+     */
+    set<int> getAdjacencies(int id);
+
+    /**
+     * Indica si 2 vèrtexs existents són adjacents.
+     * @param id1   id del 1r vèrtex.
+     * @param id2   id del 2n vèrtex.
+     * @return      true si els vètexs són adjacents, false altrament.
+     */
+    bool adjacent(int id1, int id2);
+};
+
+#endif

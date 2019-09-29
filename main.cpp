6809a57a79dfa213f2e74c17e3c13d8a6a2c1482
Oriol Pascual <oriolpascual9@gmail.com>
2019-09-29 18:45:49 +0200

    make & main

--- main.cpp
+++ main.cpp
@@ -16,17 +16,30 @@
 int main(int argc, const char * argv[]) {
 
     int n;
+    std::cout << "Introduce el tamaño del grafo" << endl;
     std::cin >> n;
 
     MyGraph G;
     std::ofstream fout("data.txt");
 
-    for (int i = 0; i < 1000; ++i) {
-        G = Generator::random_geometric_graph(n, i/1000.0);
-        // G = Generator::binomial_random_graph(n, i/1000.0);
-        fout << i/1000.0 << '\t' << Graph_Algorithms::getNrConectedComponents(G).first << endl;
-    }
+    int op;
+    std::cout << "Introduce el tipo de generador de grafos: " << endl;
+    std::cout << "	1. RandomGeometricGraph" << endl;
+	std::cout << "	2. BinominalRandomGraph" << endl;
+	std:cin >> op;
 
+	if (op == 1){
+	    for (int i = 0; i < 1000; ++i) {
+	        G = Generator::random_geometric_graph(n, i/1000.0);
+	        fout << i/1000.0 << '\t' << Graph_Algorithms::getNrConectedComponents(G).first << endl;
+	    }
+	}
+	if (op == 2){
+	    for (int i = 0; i < 1000; ++i) {
+	        G = Generator::binomial_random_graph(n, i/1000.0);
+	        fout << i/1000.0 << '\t' << Graph_Algorithms::getNrConectedComponents(G).first << endl;
+	    }
+	}
     return 0;
 }
 
